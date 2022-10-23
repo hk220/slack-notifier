e1d5713769d11b29d88467e655fb163156ed5742
Kazuki Hara <kazuki.hara.dw3@gmail.com>
2022-10-23 09:04:59 +0900

    Remove subcommands

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"github.com/hk220/slack-sender/converter"
+	"github.com/hk220/slack-sender/reader"
+	"github.com/hk220/slack-sender/sender"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -10,8 +13,44 @@ var (
 
 	rootCmd = &cobra.Command{
 		Use: "slack-sender",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			s := sender.NewSlackSender(viper.GetString("token"))
+			r := reader.NewStdinReader()
+			c := converter.NewSlackConverter(viper.GetString("username"), viper.GetString("channel"))
+
+			sc := NewSendCommand(s, r, c)
+
+			return sc.Execute()
+		},
 	}
 )
+
+type SendCommand struct {
+	Sender    sender.Sender
+	Reader    reader.Reader
+	Converter converter.Converter
+}
+
+func NewSendCommand(s sender.Sender, r reader.Reader, c converter.Converter) *SendCommand {
+	return &SendCommand{
+		Sender:    s,
+		Reader:    r,
+		Converter: c,
+	}
+}
+
+func (ss *SendCommand) Execute() error {
+	text, err := ss.Reader.Read()
+	if err != nil {
+		return err
+	}
+
+	msg := ss.Converter.Convert(text)
+	if err := ss.Sender.Send(msg); err != nil {
+		return err
+	}
+	return nil
+}
 
 func Execute() error {
 	return rootCmd.Execute()
@@ -23,6 +62,10 @@ func init() {
 	rootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "slack-sender.toml", "config file")
 	viper.BindPFlag("config", rootCmd.PersistentFlags().Lookup("config"))
 
-	rootCmd.AddCommand(sendCmd)
-	rootCmd.AddCommand(initCmd)
+	rootCmd.PersistentFlags().String("token", "", "token")
+	rootCmd.PersistentFlags().String("username", "", "username")
+	rootCmd.PersistentFlags().String("channel", "", "channel")
+	viper.BindPFlag("token", rootCmd.PersistentFlags().Lookup("token"))
+	viper.BindPFlag("username", rootCmd.PersistentFlags().Lookup("username"))
+	viper.BindPFlag("channel", rootCmd.PersistentFlags().Lookup("channel"))
 }
